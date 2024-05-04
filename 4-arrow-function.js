30f177d5191503b33da5ff35d4813c24ae9670f2
Mahir7878 <mahirmahir8951@gmail.com>
2024-05-04 17:50:24 +0530

    {

---
+++ 4-arrow-function.js
@@ -0,0 +1,21 @@
+// Arrow fundtion
+
+function multiplyByTwo1(input){return input *2};
+
+const multiplyByTwo2 = (input) => { input*2};
+
+const multiplyByTwo3 = (input) => input*2;
+
+const multiplyByTwo4 = input => input *2;
+
+function copyArrayAndManipulate(arr,instruction){
+    let output = [];
+    for(let i = 0; i < arr.length; i++){
+        output.push(instruction(arr[i]))
+    }
+    return output;
+}
+
+let array = [1,2,3];
+const result = copyArrayAndManipulate(array,multiplyByTwo4);
+console.log(result);
