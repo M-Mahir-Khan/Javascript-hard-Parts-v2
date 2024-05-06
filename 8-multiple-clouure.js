8de7eae74b56ee2344fcc0b777c070a2c86c39dc
Mahir7878 <mahirmahir8951@gmail.com>
2024-05-06 19:56:07 +0530

    this repo shows how javascript works under the hood

---
+++ 8-multiple-clouure.js
@@ -0,0 +1,20 @@
+function outer(){
+    let counter = 0;
+    function incrementCounter(){
+        return ++counter
+    }
+    return incrementCounter
+}
+
+let myNewfunc = outer();
+
+console.log(myNewfunc());   // 1
+console.log(myNewfunc());   // 2
+
+let anotherFunc = outer()
+
+console.log(anotherFunc()); // 1
+console.log(anotherFunc()); // 2
+
+console.log(outer()());     // 1
+console.log(outer()());     // 1
